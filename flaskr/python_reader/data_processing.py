b31fa89643a68250a38fba16a930bc1d6e34d7d6
Patryk <patryk26g@gmail.com>
2023-06-19 22:56:46 +0200

    Data display

--- flaskr/python_reader/data_processing.py
+++ flaskr/python_reader/data_processing.py
@@ -1,8 +1,14 @@
 import json
 import io
 import base64
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+
+
+
 
 
 class DataProcessing:
@@ -10,57 +16,43 @@ class DataProcessing:
     BASIC_TEMP_VALUE = 36.6
 
     def __init__(self):
-        with open('python_reader/data.json') as file:
-            data = json.load(file)
+        with open('../flaskr/python_reader/data.pkl', 'rb') as file:
+            data = pickle.load(file)
+            file.close()
 
-        self.lm35_temp_list = []
-        self.max_temp_list = []
-        self.max_spo2_list = []
-        self.max_bpm_list = []
-        self.x_list = []
-        self.y_list = []
-        self.z_list = []
+        max_lm35_data = data[data[:, 0] == 0.0]
+        ada_data = data[data[:, 0] == 1.0]
 
-        # Accessing the data and storing values into lists
-        for item in data:
-            lm35_temp = float(item['lm35_temp'])
-            max_temp = float(item['max_temp'])
-            max_spo2 = float(item['max_spo2'])
-            max_bpm = float(item['max_bpm'])
-            x = float(item['x'])
-            y = float(item['y'])
-            z = float(item['z'])
+        self.lm35_temp = max_lm35_data[:, 1]
+        self.max_spo2 = max_lm35_data[:, 3]
+        self.max_bpm = max_lm35_data[:, 4]
 
-            self.lm35_temp_list.append(lm35_temp)
-            self.max_temp_list.append(max_temp)
-            self.max_spo2_list.append(max_spo2)
-            self.max_bpm_list.append(max_bpm)
-            self.x_list.append(x)
-            self.y_list.append(y)
-            self.z_list.append(z)
+        self.x = ada_data[:, 2]
+        self.y = ada_data[:, 3]
+        self.z = ada_data[:, 4]
 
-
-    def get_headers(self):
-        return ['Temperature (LM35)', 'Temperature (MAX)', 'spO2 (MAX)', 'BPM (MAX)', 'X', 'Y', 'Z']
+        self.data_max = [self.lm35_temp, self.max_spo2, self.max_bpm]
+        self.data_xyz = [self.x, self.y, self.z]
+        self.data_max = [(self.data_max[0][i], self.data_max[1][i], self.data_max[2][i]) for i in
+                         range(len(self.data_max[0]))]
+        self.data_xyz = [(self.data_xyz[0][i], self.data_xyz[1][i], self.data_xyz[2][i]) for i in
+                         range(len(self.data_xyz[0]))]
 
     def get_data(self):
-        return [self.lm35_temp_list,
-                self.max_temp_list,
-                self.max_spo2_list,
-                self.max_bpm_list,
-                self.x_list,
-                self.y_list,
-                self.z_list]
+        return self.data_max, self.data_xyz
+
+    def get_headers(self):
+        return ['Temperature (LM35)', 'spO2 (MAX)', 'BPM (MAX)'], ['X', 'Y', 'Z']
 
     def get_plots(self):
-        mean_spo2 = np.mean(self.max_spo2_list)
-        std_spo2 = np.std(self.max_spo2_list)
+        mean_spo2 = np.mean(self.max_spo2)
+        std_spo2 = np.std(self.max_spo2)
 
         values = [mean_spo2, self.BASIC_SPO2_LEVEL]
         std = [std_spo2, 0]
 
         # Data for the second plot
-        healthy_temp = [self.BASIC_TEMP_VALUE for _ in range(len(self.lm35_temp_list))]
+        healthy_temp = [self.BASIC_TEMP_VALUE for _ in range(len(self.lm35_temp))]
 
         plt.bar(range(len(values)), values, yerr=std, align='center', alpha=0.6)
         plt.xlabel('X1')
@@ -77,13 +69,13 @@ class DataProcessing:
         plt.close()  # Close the plot to free up resources
 
         # Temperature values
-        plt.plot(range(len(self.lm35_temp_list)), self.lm35_temp_list, marker='o', label='Measured values')
+        plt.plot(range(len(self.lm35_temp)), self.lm35_temp, marker='o', label='Measured values')
         plt.plot(range(len(healthy_temp)), healthy_temp, label='Healthy value')
         plt.xlabel('Measurement')
         plt.ylabel('Temperature')
         plt.title('Temperature Values')
-        plt.xticks(range(len(self.lm35_temp_list)), [str(i) for i in range(len(self.lm35_temp_list))])
-        plt.ylim(min(36, int(min(self.lm35_temp_list) - 0.5)), max(38, int(max(self.lm35_temp_list) + 1)))
+        plt.xticks(range(len(self.lm35_temp)), [str(i) for i in range(len(self.lm35_temp))])
+        plt.ylim(min(36, int(min(self.lm35_temp) - 0.5)), max(38, int(max(self.lm35_temp) + 1)))
         plt.legend()
 
         # Save Plot 2 to a buffer
