5e161b4a4e4ca76f7a3673d778f7c50aa010bc77
Patryk <patryk26g@gmail.com>
2023-06-23 19:00:36 +0200

    Data display 2

--- flaskr/flask_app.py
+++ flaskr/flask_app.py
@@ -9,66 +9,116 @@ from python_reader.data_processing import DataProcessing
 
 
 class MyApp:
+    BASIC_SPO2_LEVEL = 97
+    BASIC_TEMP_VALUE = 36.6
+
     def __init__(self):
         self.app = Flask(__name__, template_folder='templates')
         self.app.debug = True
         self.app.static_folder = 'static'
         self.data_processing = DataProcessing()
-        self.data_max, self.data_xyz = self.data_processing.get_data()
-        self.header_max, self.header_xyz = self.data_processing.get_headers()
+        self.max_bpm, self.max_spo2, self.lm35_temp, self.data_max, self.data_xyz = self.data_processing.get_data()
         self.i_xyz = 0
         self.i_max = 0
 
         @self.app.route('/')
         def display_table_and_plot():
-            plot_data1, plot_data2 = self.data_processing.get_plots()
-            return render_template('main_page.html',
-                                   data_max=self.data_max,
-                                   data_xyz=self.data_xyz,
-                                   header_max=self.header_max,
-                                   header_xyz=self.header_xyz,
-                                   plot_data1=plot_data1,
-                                   plot_data2=plot_data2)
+            return render_template('main_page.html')
 
-        @self.app.route('/get_xyz')
-        def get_xyz():
-            return jsonify(values=self.get_xyz_())
+        @self.app.route('/get_spO2_plot')
+        def get_spo2_plot():
+            error = False
+            mean_spo2 = np.mean(self.max_spo2[self.max_spo2 != -1])
+            std_spo2 = np.std(self.max_spo2[self.max_spo2 != -1])
+            if np.isnan(mean_spo2):
+                mean_spo2 = 0
+                std_spo2 = 0
+                error = True
+            values = [mean_spo2, self.BASIC_SPO2_LEVEL]
+            std = [std_spo2, 0]
 
-        @self.app.route('/get_max')
-        def get_max():
-            return jsonify(values=self.get_max_())
+            fig, ax = plt.subplots(1, 1)
+            ax.bar(range(len(values)), values, yerr=std, align='center', alpha=0.6)
+            ax.set_xlabel('X1')
+            ax.set_ylabel('SpO2 (%)')
+            ax.set_title('Level of SpO2')
+            ax.set_ylim(80, 100)
+            ax.set_xticks(range(len(values)), ['Measured value', 'Healthy value'])
+
+            buffer = io.BytesIO()
+            fig.savefig(buffer, format='png')
+            buffer.seek(0)
+            plot = base64.b64encode(buffer.read()).decode()
+            plt.close(fig)
+            data = {'healthy': self.BASIC_SPO2_LEVEL, 'measured': mean_spo2, 'std': std_spo2, 'error': error}
+            print(data)
+
+            return jsonify(plot=plot, data=data)
+
+        @self.app.route('/get_temp_plot')
+        def get_temp_plot():
+            healthy_temp = [self.BASIC_TEMP_VALUE for _ in range(len(self.lm35_temp))]
+
+            fig, ax = plt.subplots(1, 1)
+            # Temperature values
+            ax.plot(range(len(self.lm35_temp)), self.lm35_temp, marker='o', label='Measured values')
+            ax.plot(range(len(healthy_temp)), healthy_temp, label='Healthy value')
+            ax.set_xlabel('Measurement')
+            ax.set_ylabel('Temperature')
+            ax.set_title('Temperature Values')
+            ax.set_xticks(range(len(self.lm35_temp)), [str(i) for i in range(len(self.lm35_temp))])
+            ax.set_ylim(min(36, int(min(self.lm35_temp) - 0.5)), max(38, int(max(self.lm35_temp) + 1)))
+            ax.legend()
+            buffer = io.BytesIO()
+            fig.savefig(buffer, format='png')
+            buffer.seek(0)
+            plot = base64.b64encode(buffer.read()).decode()
+            plt.close(fig)
+            data = {'healthy': self.BASIC_TEMP_VALUE, 'measured': np.mean(self.lm35_temp)}
+
+            return jsonify(plot=plot, data=data)
 
         @self.app.route('/get_xyz_plot')
-        def get_plots():
+        def get_xyz_plot():
             xyz_value = self.get_xyz_()
+            std_values = np.std(xyz_value, axis=0)
+            std_values = {'x':std_values[0], 'y':std_values[0], 'z':std_values[0]}
+            max_value = np.max(np.max(xyz_value, axis=0))
+            min_value = np.min(np.min(xyz_value, axis=0))
 
-            plt.plot(xyz_value)
-            plt.ylabel('Wychylenie')
-            plt.title('X, Y, Z')
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(xyz_value)
+            ax.set_ylabel('Wychylenie')
+            ax.set_title('X, Y, Z')
+            ax.set_ylim(min(min_value, -3), max(max_value, 3))
+            ax.legend(['X', 'Y', 'Z'], loc='upper right')
 
             buffer_x = io.BytesIO()
-            plt.savefig(buffer_x, format='png')
+            fig.savefig(buffer_x, format='png')
             buffer_x.seek(0)
-            plot_x = base64.b64encode(buffer_x.read()).decode()
-            plt.close()  # Close the plot to free up resources
+            plot = base64.b64encode(buffer_x.read()).decode()
+            plt.close(fig)  # Close the plot to free up resources
 
-            return jsonify(plot=plot_x)
+            return jsonify(plot=plot, std_values=std_values)
 
     def get_max_(self):
         values = self.data_max
         values_len = len(self.data_max)
-        values = values[self.i_max % values_len: self.i_max % values_len + 30]
-        if values_len > 30:
+        values = values[self.i_max % values_len: self.i_max % values_len + 15]
+        if values_len > 10:
             self.i_max += 1
         return values
 
     def get_xyz_(self):
         values = self.data_xyz
         values_len = len(self.data_xyz)
-        values = values[self.i_xyz % values_len: self.i_xyz % values_len + 30]
-        if values_len > 30:
+        values = values[self.i_xyz % values_len: self.i_xyz % values_len + 15]
+        if values_len > 10:
             self.i_xyz += 1
+        if self.i_xyz + 15 >= values_len:
+            self.i_xyz = 0
         return values
+
     def run(self):
         self.app.static_folder = 'static'
         self.app.debug = True
@@ -76,6 +126,4 @@ class MyApp:
 
 
 my_app = MyApp()
-# data_update_thread = threading.Thread(target=my_app.update_data_xyz)
-# data_update_thread.start()
 my_app.run()
--- flaskr/static/track_changes.js
+++ flaskr/static/track_changes.js
@@ -1,70 +1,96 @@
-function updateXYZ() {
-            $.ajax({
-                url: '/get_xyz',
-                success: function(response) {
-                    var values = response.values;
+updateXYZPlot()
+updateSpO2Plot()
+updateTempPlot()
 
-                    var valueTable = $('#xyz-table');
-                    var valueBody = $('#xyz-body');
-                    valueBody.empty();  // Clear the existing table body
-
-                    // Iterate over the values array and generate rows
-                    values.forEach(function(row) {
-                        var newRow = $('<tr>');
-                        row.forEach(function(value) {
-                            var newCell = $('<td>').text(value);
-                            newRow.append(newCell);
-                        });
-                        valueBody.append(newRow);
-                    });
-
-                    // Scroll to the bottom of the table
-                    valueTable.scrollTop(valueTable.prop("scrollHeight"));
-                }
-            });
-        }
-        setInterval(updateXYZ, 100);
-
-function updateMAX() {
-            $.ajax({
-                url: '/get_max',
-                success: function(response) {
-                    var values = response.values;
-
-                    var valueTable = $('#max-table');
-                    var valueBody = $('#max-body');
-                    valueBody.empty();  // Clear the existing table body
-
-                    // Iterate over the values array and generate rows
-                    values.forEach(function(row) {
-                        var newRow = $('<tr>');
-                        row.forEach(function(value) {
-                            var newCell = $('<td>').text(value);
-                            newRow.append(newCell);
-                        });
-                        valueBody.append(newRow);
-                    });
-
-                    // Scroll to the bottom of the table
-                    valueTable.scrollTop(valueTable.prop("scrollHeight"));
-                }
-            });
-        }
-        setInterval(updateMAX, 1000);
-
-function updateXPlot() {
+function updateXYZPlot() {
             $.ajax({
                 url: '/get_xyz_plot',
                 success: function(response) {
-                    var plot = response.plot;
+                    let plot = response.plot;
+                    let imageSrc = 'data:image/png;base64,' + plot;
+                    $('#xyz-plot-image').attr('src', imageSrc);
 
-                    // Convert the base64-encoded plot to an image source
-                    var imageSrc = 'data:image/png;base64,' + plot;
+                    let std_values = response.std_values;
+                    let x = parseFloat(std_values['x'].toFixed(2))
+                    let y = parseFloat(std_values['y'].toFixed(2))
+                    let z = parseFloat(std_values['z'].toFixed(2))
 
-                    // Update the plot image on the page
-                    $('#plot-image').attr('src', imageSrc);
+                    let warning = $('#xyz-warning')
+                    if (z > 0.2 && y > 0.2) {
+                        warning.html('Warning! Possible asthma attack!')
+                        warning.css('color', 'red');
+                        warning.css('font-weight', 'bold');
+                    } else {
+                        warning.html('Breath seems to be normal')
+                        warning.css('color', 'green');  // Change the text color to red
+                    }
 
                 }
             });
         }
-        setInterval(updateXPlot, 100);
+        setInterval(updateXYZPlot, 100);
+
+function updateSpO2Plot() {
+            $.ajax({
+                url: '/get_spO2_plot',
+                success: function(response) {
+                    let plot = response.plot;
+                    let imageSrc = 'data:image/png;base64,' + plot;
+                    $('#spO2-plot-image').attr('src', imageSrc);
+
+                    let data = response.data;
+                    let healthy = data['healthy']
+                    let measured = data['measured']
+                    let std = data['std']
+                    let error = data['error']
+                    let warning = $('#spO2-warning')
+                    console.log(data)
+
+                    if (error) {
+                        warning.html('Warning! SpO2 level couldn\'t be measured!')
+                        warning.css('color', 'red');
+                        warning.css('font-weight', 'bold');
+                    } else if (healthy < measured + std) {
+                        warning.html('SpO2 level is OK')
+                        warning.css('color', 'green');  // Change the text color to red
+                    } else {
+                        warning.html('Warning! SpO2 level is too low!')
+                        warning.css('color', 'red');
+                        warning.css('font-weight', 'bold');
+                    }
+                }
+            });
+        }
+        setInterval(updateSpO2Plot, 4000);
+
+function updateTempPlot() {
+            $.ajax({
+                url: '/get_temp_plot',
+                success: function(response) {
+                    let plot = response.plot;
+                    let imageSrc = 'data:image/png;base64,' + plot;
+                    $('#temp-plot-image').attr('src', imageSrc);
+
+                    let data = response.data;
+                    let warning = $('#temp-warning')
+                    let diff = data['healthy'] - data['measured']
+
+                    if (diff > 1) {
+                        warning.html('Warning! Temperature too low')
+                        warning.css('color', 'red');
+                        warning.css('font-weight', 'bold');
+                    } else if(diff < -1) {
+                        warning.html('Warning! Temperature too high')
+                        warning.css('color', 'red');
+                        warning.css('font-weight', 'bold');
+                    }
+                    else {
+                        warning.html('Temperature is OK')
+                        warning.css('color', 'green');
+                    }
+
+
+                }
+            });
+        }
+        setInterval(updateTempPlot, 4000);
